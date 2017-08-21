69965e2affb09bbb124c2a7972096ecfbb21564b
kgryte <kgryte@gmail.com>
2017-08-21 15:21:07 -0700

    Refactor script

--- tools/scripts/inline.sh
+++ tools/scripts/inline.sh
@@ -5,92 +5,106 @@
 # ## Notes
 #
 # * This utility is useful when debugging nginx configuration files and/or providing a single configuration file for deployment.
+# * In order to resolve relative `include` statements, you must invoke this script in the directory in which the configuration file is located.
 #
 
-PROGNAME=${0##*/}
-PROGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-FILES=()
 
-error_exit() {
-	echo -e "${PROGNAME}: ${1:-"Unknown Error"}" >&2
-	exit 1
+# VARIABLES #
+
+# Determine the script name:
+script_name="${0##*/}"
+
+# Initialize a file list:
+files=()
+
+
+# FUNCTIONS #
+
+# Defines an error handler.
+#
+# $1 - error status
+on_error() {
+	cleanup
+	exit "$1"
 }
 
-graceful_exit() {
-	exit
+# Runs clean-up tasks.
+cleanup() {
+	echo '' >&2
 }
 
+# Prints usage information.
 usage() {
-	echo -e "Usage: $PROGNAME [-h|--help] <filename>"
+	echo '' >&2
+	echo "Usage: ${script_name} [options] filename" >&2
+	echo '' >&2
+	echo 'Replace include statements with the included files. The primary use case for' >&2
+	echo 'this script is to debug nginx configuration files and/or to provide a single' >&2
+	echo 'configuration file for distribution/deployment.' >&2
+	echo '' >&2
+	echo 'Options:' >&2
+	echo '' >&2
+	echo '  -h,    --help                Print this message.' >&2
+	echo '' >&2
+	echo 'Example:' >&2
+	echo '' >&2
+	echo '  $ cd /etc/nginx' >&2
+	echo "  \$ ${script_name} nginx.conf" >&2
+	echo '' >&2
 }
 
-help_message() {
-	cat <<- _EOF_
-	$PROGNAME
-	Replace include statements with the included files.
-
-	The primary use of this script is for debugging nginx
-	config files and/or to provide a single config file for
-	distribution/deployment.
-
-	$(usage)
-
-	Example:
-		cd /etc/nginx
-		$PROGNAME nginx.conf
-		### nginx.conf START
-
-		### directive-only/x-ua-compatible.conf START
-		add_header "X-UA-Compatible" "IE=Edge";
-		....
-
-	Options:
-	-h, --help  Display this message.
-
-_EOF_
-	return
-}
-
+# Main execution sequence.
+#
+# $1 - files
 main() {
-		echo "### $1 START"
-		while read line
-		do
-				if [[ $line =~ "#" ]];
-				then
-						# Skip comments...
-						continue
-				elif [[ $line =~ "^ *$" ]];
-				then
-						# Skip blank lines...
-						continue
-				elif [[ $line =~ ^(\s*)include\s*(.*)\; ]];
-				then
-						main ${BASH_REMATCH[2]}
-				else
-						echo "$line"
-				fi
-		done < $1
-		echo "### $1 END"
+	echo "### $1 START"
+	while read line; do
+		if [[ "${line}" =~ "#" ]]; then
+			# Skip comments...
+			continue
+		elif [[ "${line}" =~ "^ *$" ]]; then
+			# Skip blank lines...
+			continue
+		elif [[ "${line}" =~ ^(\s*)include\s*(.*)\; ]]; then
+			main ${BASH_REMATCH[2]}
+		else
+			echo "${line}"
+		fi
+	done < "$1"
+	echo "### $1 END"
 }
 
-# Parse command-line arguments:
-while [[ -n $1 ]]; do
-	case $1 in
-		-h | --help)
-			help_message; graceful_exit ;;
-		-* | --*)
+# Parse command-line options...
+while [[ -n "$1" ]]; do
+	case "$1" in
+		'-h' | '--help')
 			usage
-			error_exit "Unknown option $1" ;;
+			exit 0
+			;;
+
+		'--')
+			# End of all options:
+			shift
+			break
+			;;
+
+		-?*)
+			printf 'ERROR: unknown option: %s\n' "$1" >&2
+			on_error 1
+			;;
 		*)
-			FILES+=($1);;
+			# Default case (e.g., if no more options) is a file name:
+			files+=("$1")
+			;;
 	esac
 	shift
 done
 
-if [ ${#FILES[@]} -eq 0 ];
-then
-			usage;
-			graceful_exit;
+# If we are not provided one or more configuration files, print the help text...
+if [[ "${#files[@]}" -eq 0 ]]; then
+	usage
+	exit 0
 fi
 
-main $FILES
+# Run main:
+main "${files}"
