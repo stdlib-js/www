5c99219b4b6058a2fb8354783259930d1fe943d5
kgryte <kgryte@gmail.com>
2017-09-04 23:00:52 -0700

    Add GA with support for doNotTrack

--- TODO.md
+++ TODO.md
@@ -10,9 +10,6 @@
 3. landing page
 
    - main website
-
-     * fix top nav on mobile
-
    - docs specific
 
 4. GitHub pages redirect
@@ -33,28 +30,28 @@
 
    - should add a privacy policy to the repo describing what data is collected and why and should link to this policy in the docs (footer link)
 
-8. view which transforms slideout menu into a fullscreen mega menu overlay (maybe with a touch of transparency, so can see the page update underneath)
-9. build process which selectively updates only modified docs (i.e., avoid full build every time a README is updated; should be able to only rebuild and update that README)
-10. update docs top-nav to match landing page (menu bar styling etc, fonts, box shadow, etc)
+8. (docs) view which transforms slideout menu into a fullscreen mega menu overlay (maybe with a touch of transparency, so can see the page update underneath)
+9. (docs) build process which selectively updates only modified docs (i.e., avoid full build every time a README is updated; should be able to only rebuild and update that README)
+10. (docs) update docs top-nav to match landing page (menu bar styling etc, fonts, box shadow, etc)
 11. diffie-hellman: https://mozilla.github.io/server-side-tls/ssl-config-generator/
 
     - see digital ocean blog
 
 12. SSL-stapling config
-13. HTML minify (rehype plugin)
-14. CSS and JavaScript in HTML minify (rehype plugin)
-15. Consider JS bundle minify
-16. Replace `@stdlib/stdlib` HTML doc with something less repo/dev focused
-17. Determine whether to delineate the doc version with `.` or with `_` in URL
-18. add support for different build targets (e.g., a `www` target where we can refer to common assets like `reset.css`, package ToC, etc, and a standalone target which inlines everything such that package docs can be standalone)
-19. investigate [hammer.js](https://github.com/hammerjs/hammer.js) for touch support
+13. (docs) HTML minify (rehype plugin)
+14. (docs) CSS and JavaScript in HTML minify (rehype plugin)
+15. (docs) Consider JS bundle minify
+16. (docs) Replace `@stdlib/stdlib` HTML doc with something less repo/dev focused
+17. (docs) Determine whether to delineate the doc version with `.` or with `_` in URL
+18. (docs) add support for different build targets (e.g., a `www` target where we can refer to common assets like `reset.css`, package ToC, etc, and a standalone target which inlines everything such that package docs can be standalone)
+19. (docs) investigate [hammer.js](https://github.com/hammerjs/hammer.js) for touch support
 
     - swipe right to reveal slideout menu
     - swipe left to close slideout menu
 
-20. resolve source URLs (see note in build-package/lib/build.js)
-21. sidenote/marginnote styling: https://edwardtufte.github.io/tufte-css/
-22. host the exact Lato font-weight we need for the side menu (200), rather than rely on Google (if we place the website behind a CDN, we should be okay)
+20. (docs) resolve source URLs (see note in build-package/lib/build.js)
+21. (docs) sidenote/marginnote styling: https://edwardtufte.github.io/tufte-css/
+22. (docs) host the exact Lato font-weight we need for the side menu (200), rather than rely on Google (if we place the website behind a CDN, we should be okay)
 
     - need to update font links to hosted resources
 
---
+++ public/js/google_analytics.js
@@ -0,0 +1,110 @@
+/*
+* Original implementation attributed to Schalk Neethling (see {@link https://github.com/schalkneethling/dnt-helper}). The embedded license applies strictly to the `dnt()` function.
+*
+* The implementation, as included here, has been modified from the original.
+*/
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Schalk Neethling
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+(function() {
+	var RE_WINDOWS = /Windows.+?(?=;)/g; // matches from Windows up to the first occurrence of `;` non-greedily
+	var RE_FIREFOX = /Firefox\/(\d+)/;
+	var RE_IE = /MSIE|Trident/i;
+
+	// List of Windows versions known to not implement DNT according to the standard.
+	var anomalousWinVersions = [
+		'Windows NT 6.1',
+		'Windows NT 6.2',
+		'Windows NT 6.3'
+	];
+
+	/**
+	* Returns a boolean indicating whether `doNotTack` (DNT) is enabled.
+	*
+	* ## Notes
+	*
+	* * The function takes into account anomalies, such as !bugzilla 887703, which affect versions of Firefox 31 and lower.
+	* * The function handles IE versions on Windows 7, 8 and 8.1, where the DNT implementation does not honor the spec.
+	*
+	*
+	* [1]: https://bugzilla.mozilla.org/show_bug.cgi?id=1217896
+	* [2]: link http://schalkneethling.github.io/blog/2015/11/06/respect-user-choice-do-not-track/
+	* [3]: https://github.com/schalkneethling/dnt-helper/blob/master/js/dnt-helper.js
+	*
+	* @private
+	* @returns {boolean} boolean indicating whether `doNotTrack` is enabled
+	*/
+	function dnt() {
+		'use strict';
+
+		var platform;
+		var status;
+		var lookup;
+		var match;
+		var isIE;
+		var ua;
+
+		// Browser value lookup table:
+		lookup = {
+			'0': false, // disabled
+			'1': true // enabled
+		};
+
+		// For old versions of IE, we need to use the `msDoNotTrack` property of `navigator`. On newer versions, and newer platforms, this is `doNotTrack` but, on the `window` object. Safari also exposes the property on the `window` object.
+		status = navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack;
+		ua = navigator.userAgent;
+
+		isIE = RE_IE.test( ua );
+
+		// With old versions of IE, DNT did not exist so we simply return false;
+		if ( isIE && typeof Array.prototype.indexOf !== 'function') {
+			return false;
+		}
+		match = ua.match( RE_FIREFOX );
+		if ( match && parseInt( match[ 1 ], 10 ) < 32 ) {
+			// Cannot say for sure if it is 1 or 0, due to Firefox bug 887703...
+			return false; // "Unspecified"
+		}
+		platform = ua.match( RE_WINDOWS );
+		if ( isIE && platform && anomalousWinVersions.indexOf( platform[ 0 ] ) !== -1 ) {
+			// Default is on, which does not honor the specification...
+			return false; // "Unspecified"
+		}
+		status = lookup[ status ];
+		if ( status === void 0 ) {
+			return false;
+		}
+		return true;
+	}
+
+	if ( dnt() === false ) {
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga( 'create', 'UA-105890493-1', 'auto' );
+		ga( 'send', 'pageview' );
+	}
+})();
