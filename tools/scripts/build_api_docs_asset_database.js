8330cdc2c1921eb9cbcb8d6130e005be94f7fb9a
kgryte <kgryte@gmail.com>
2019-11-21 17:34:41 -0800

    Update and add build scripts

---
+++ tools/scripts/build_api_docs_asset_database.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+// MODULES //
+
+var join = require( 'path' ).join;
+var flattenObject = require( '@stdlib/utils/flatten-object' );
+var merge = require( '@stdlib/utils/merge' );
+var objectKeys = require( '@stdlib/utils/keys' );
+var exists = require( '@stdlib/fs/exists' ).sync;
+var writeFile = require( '@stdlib/fs/write-file' ).sync;
+var readJSON = require( '@stdlib/fs/read-json' ).sync;
+var documentationPath = require( './api_docs_path.js' );
+
+
+// VARIABLES //
+
+var OUTPUT = 'package_resources.json';
+
+
+// MAIN //
+
+/**
+* Main execution sequence.
+*
+* @private
+* @throws {Error} unexpected error
+*/
+function main() {
+	var dpath;
+	var opts;
+	var tree;
+	var keys;
+	var db;
+	var k;
+	var i;
+
+	// Resolve the API documentation path:
+	dpath = documentationPath();
+
+	// Load the API documentation package tree:
+	opts = {
+		'encoding': 'utf8'
+	};
+	tree = readJSON( join( dpath, 'package_tree.json' ), opts );
+	if ( tree instanceof Error ) {
+		throw tree;
+	}
+	// Build an API documentation asset database...
+	opts = {
+		'delimiter': '/',
+		'depth': 0
+	};
+	db = flattenObject( tree, opts );
+	while ( opts.depth <= 6 ) {
+		db = merge( db, flattenObject( tree, opts ) );
+		opts.depth += 1;
+	}
+	keys = objectKeys( db );
+	for ( i = 0; i < keys.length; i++ ) {
+		k = keys[ i ];
+		db[ k ] = {
+			'benchmark': exists( join( dpath, '@stdlib', k, 'benchmark.html' ) ),
+			'test': exists( join( dpath, '@stdlib', k, 'test.html' ) )
+		}
+	}
+	// Write the database to file:
+	writeFile( join( dpath, OUTPUT ), JSON.stringify( db ) );
+}
+
+main();
