eae20462a9863b19f9520904795f7de217c5bf9d
kgryte <kgryte@gmail.com>
2019-12-13 11:36:51 -0800

    Add missing license header and convert function type

--- src/generate_html_boilerplate.jsx
+++ src/generate_html_boilerplate.jsx
@@ -1,3 +1,21 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2019 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 // VARIABLES //
 
 const IFRAME_RESIZER_CONTENT_SCRIPT_PATH = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.2.1/iframeResizer.contentWindow.min.js';
@@ -12,7 +30,8 @@ const IFRAME_RESIZER_CONTENT_SCRIPT_PATH = 'https://cdnjs.cloudflare.com/ajax/li
 * @param {string} resourcePath - URL of resource to fetch
 * @returns {string} boilerplate HTML page
 */
-const generateHTMLBoilerplate = ( resourcePath ) => `<html>
+function generateHTMLBoilerplate( resourcePath ) {
+	return `<html>
 <body>
 <script type="text/javascript">
 document.addEventListener( 'DOMContentLoaded', () => {
@@ -31,6 +50,7 @@ document.addEventListener( 'DOMContentLoaded', () => {
 </script>
 </body>
 </html>`;
+}
 
 
 // EXPORTS //
