6597a3ef5dd938ca213b01d814dd37f800ba636f
Athan Reines <kgryte@gmail.com>
2021-09-06 02:11:08 -0700

    Refactor as a class component

--- src/components/top-nav/search_input.jsx
+++ src/components/top-nav/search_input.jsx
@@ -30,35 +30,119 @@ import SearchIcon from '@material-ui/icons/Search';
 * Component for displaying a search input element.
 *
 * @private
-* @param {Object} props - component properties
-* @param {Callback} props.onChange - callback to invoke upon a change to the search input element
-* @param {Callback} props.onKeyUp - callback to invoke upon a user releasing a key on a keyboard
-* @param {Callback} props.onSubmit - callback to invoke upon a user attempting to submit a search query
-* @returns {ReactElement} React element
 */
-function SearchInput( props ) {
-	return (
-		<Fragment>
-			<InputBase
-				className="top-nav-search"
-				placeholder="Search documentation"
-				type="text"
-				inputProps={{
-					'aria-label': 'search documentation'
-				}}
-				onChange={ props.onChange }
-				onKeyUp={ props.onKeyUp }
-			/>
-			<IconButton
-				type="button"
-				className="icon-button top-nav-search-button"
-				aria-label="search"
-				onClick={ props.onSubmit }
-			>
-				<SearchIcon />
-			</IconButton>
-		</Fragment>
-	);
+class SearchInput extends React.Component {
+	/**
+	* Returns a component for displaying a search input element.
+	*
+	* @private
+	* @constructor
+	* @param {Object} props - component properties
+	* @param {Callback} props.onSubmit - callback to invoke upon a user submitting a search query
+	* @returns {ReactComponent} React component
+	*/
+	constructor( props ) {
+		super( props );
+		this.state = {
+			// Search query:
+			'query': '',
+
+			// Boolean indicating whether a search input element is "active" (e.g., is focused or contains a search query):
+			'active': false
+		};
+	}
+
+	/**
+	* Callback invoked upon updating a search input element.
+	*
+	* @private
+	* @param {Object} event - event object
+	*/
+	_onChange = ( event ) => {
+		this.setState({
+			'query': event.target.value
+		});
+	}
+
+	/**
+	* Callback invoked upon a user releasing a key on a keyboard when using a search input element.
+	*
+	* @private
+	* @param {Object} event - event object
+	* @returns {void}
+	*/
+	_onKeyUp = ( event ) => {
+		if ( this.state.query === '' ) {
+			return;
+		}
+		if ( event.charCode === 13 || event.key === 'Enter' ) {
+          this.props.onSubmit( this.state.query );
+        }
+	}
+
+	/**
+	* Callback invoked upon the search input element being focused.
+	*
+	* @private
+	*/
+	_onFocus = () => {
+		this.setState({
+			'active': true
+		});
+	}
+
+	/**
+	* Callback invoked upon the search input element losing focus.
+	*
+	* @private
+	*/
+	_onBlur = () => {
+		this.setState({
+			'active': false
+		});
+	}
+
+	/**
+	* Callback invoked upon clicking a submit button.
+	*
+	* @private
+	*/
+	_onSubmitClick = () => {
+		this.props.onSubmit( this.state.query );
+	}
+
+	/**
+	* Renders the component.
+	*
+	* @private
+	* @returns {ReactElement} React element
+	*/
+	render() {
+		return (
+			<Fragment>
+				<InputBase
+					className={ 'top-nav-search ' + ( ( this.state.active || this.state.query ) ? 'top-nav-search-active' : '' ) }
+					placeholder="Search documentation"
+					type="text"
+					inputProps={{
+						'aria-label': 'search documentation'
+					}}
+					onChange={ this._onChange }
+					onKeyUp={ this._onKeyUp }
+					onFocus={ this._onFocus }
+					onBlur={ this._onBlur }
+				/>
+				<IconButton
+					type="button"
+					className="icon-button top-nav-search-button"
+					aria-label="search"
+					onClick={ this._onSubmitClick }
+				>
+					<SearchIcon />
+				</IconButton>
+			</Fragment>
+		);
+	}
 }
 
 
