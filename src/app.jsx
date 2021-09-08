56dbf52cc145d92dfa64e26d7284f34cc571e827
Planeshifter <pburckhardt@outlook.com>
2021-09-08 12:37:25 -0400

    Add link to edit README on GitHub

--- src/app.jsx
+++ src/app.jsx
@@ -489,6 +489,7 @@ class App extends React.Component {
 	* @private
 	* @param {Object} match - match object
 	* @param {string} match.url - resource URL
+	* @param {string} match.params.pkg - package name (e.g., `math/base/special/sin`)
 	* @returns {ReactElement} React element
 	*/
 	_renderReadme( match ) {
@@ -496,6 +497,7 @@ class App extends React.Component {
 			<Readme
 				html={ this._fetchFragment( match.url ) }
 				onClick={ this._onReadmeClick }
+				pkg={ match.params.pkg }
 			/>
 		);
 	}
