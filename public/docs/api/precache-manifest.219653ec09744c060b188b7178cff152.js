f30b414abc1588c3cc63eb5f3ab9fc7a944d9cab
Athan Reines <kgryte@gmail.com>
2021-09-08 15:08:10 -0700

    Rebuild app

--- public/docs/api/precache-manifest.219653ec09744c060b188b7178cff152.js
+++ public/docs/api/precache-manifest.219653ec09744c060b188b7178cff152.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c1307d55773e6b9219ac363adac4038",
+    "revision": "8567da955fc333ba04a52b224c681a5f",
     "url": "/docs/api/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/docs/api/static/js/2.a800e7ad.chunk.js"
   },
   {
-    "revision": "4edeb121e521b1d9246a",
-    "url": "/docs/api/static/js/main.85a12f84.chunk.js"
+    "revision": "d35881a3449ba162b1db",
+    "url": "/docs/api/static/js/main.9dc9e164.chunk.js"
   },
   {
     "revision": "96e3e1367d878efa8f50",
--- public/docs/api/service-worker.js
+++ public/docs/api/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/docs/api/precache-manifest.ead059e4103feea39eaef223dd58d3db.js"
+  "/docs/api/precache-manifest.219653ec09744c060b188b7178cff152.js"
 );
 
 self.addEventListener('message', (event) => {
