de01364f812e183b5d284549d1ae86a5ff62f5f0
Athan Reines <kgryte@gmail.com>
2021-09-15 01:10:58 -0700

    Add utility to return the last index of a specified character

---
+++ src/utils/last_index_of.js
@@ -0,0 +1,45 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2021 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// MAIN //
+
+/**
+* Returns the last index of a specified character.
+*
+* @private
+* @param {string} str - string to search
+* @param {string} ch - character to match
+* @returns {integer} index
+*/
+function lastIndexOf( str, ch ) {
+	var len;
+	var i;
+
+	len = str.length;
+	for ( i = len-1; i >= 0; i-- ) {
+		if ( str[ i ] === ch ) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+// EXPORTS //
+
+export default lastIndexOf;
