550d3e572a7551d20e52e173e53dfb0af371e969
Athan Reines <kgryte@gmail.com>
2021-09-17 17:29:51 -0700

    Use single quotes

--- server/index.js
+++ server/index.js
@@ -24,29 +24,29 @@ require( 'ignore-styles' );
 // Register `require` hooks to allow import JSX and other non-standard Node.js behavior when dynamically compiling the React-based web application:
 require( '@babel/register' )({
 	ignore: [ /(node_modules)/ ],
-	root:"./server",
+	root: './server',
 	plugins: [
-		"@babel/plugin-transform-react-jsx",
-		"@babel/plugin-proposal-function-bind",
+		'@babel/plugin-transform-react-jsx',
+		'@babel/plugin-proposal-function-bind',
 
-		"@babel/plugin-proposal-export-default-from",
-		"@babel/plugin-proposal-logical-assignment-operators",
-		["@babel/plugin-proposal-optional-chaining", { "loose": false }],
-		["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }],
-		["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": false }],
-		"@babel/plugin-proposal-do-expressions",
+		'@babel/plugin-proposal-export-default-from',
+		'@babel/plugin-proposal-logical-assignment-operators',
+		[ '@babel/plugin-proposal-optional-chaining', { 'loose': false } ],
+		[ '@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' } ],
+		[ '@babel/plugin-proposal-nullish-coalescing-operator', { 'loose': false } ],
+		'@babel/plugin-proposal-do-expressions',
 
-		["@babel/plugin-proposal-decorators", { "legacy": true }],
-		"@babel/plugin-proposal-function-sent",
-		"@babel/plugin-proposal-export-namespace-from",
-		"@babel/plugin-proposal-numeric-separator",
-		"@babel/plugin-proposal-throw-expressions",
+		[ '@babel/plugin-proposal-decorators', { 'legacy': true } ],
+		'@babel/plugin-proposal-function-sent',
+		'@babel/plugin-proposal-export-namespace-from',
+		'@babel/plugin-proposal-numeric-separator',
+		'@babel/plugin-proposal-throw-expressions',
 
-		"@babel/plugin-transform-modules-commonjs",
-		"@babel/plugin-syntax-dynamic-import",
-		"@babel/plugin-syntax-import-meta",
-		["@babel/plugin-proposal-class-properties", { "loose": false }],
-		"@babel/plugin-proposal-json-strings"
+		'@babel/plugin-transform-modules-commonjs',
+		'@babel/plugin-syntax-dynamic-import',
+		'@babel/plugin-syntax-import-meta',
+		[ '@babel/plugin-proposal-class-properties', { 'loose': false } ],
+		'@babel/plugin-proposal-json-strings'
 	]
 });
 
