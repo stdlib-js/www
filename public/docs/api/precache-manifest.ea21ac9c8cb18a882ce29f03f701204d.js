a8e62a0196a3828421d32d54a6276a9876aa0b47
Athan Reines <kgryte@gmail.com>
2021-09-23 02:09:20 -0700

    Rebuild app

---
+++ public/docs/api/precache-manifest.ea21ac9c8cb18a882ce29f03f701204d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebdae4894ced6a840955171836c952b",
+    "url": "/docs/api/index.html"
+  },
+  {
+    "revision": "6eb8dcee5bc0c6951967",
+    "url": "/docs/api/static/js/2.3de0186f.chunk.js"
+  },
+  {
+    "revision": "f6f9be39a02d8aaa70c3",
+    "url": "/docs/api/static/js/main.cd01c86d.chunk.js"
+  },
+  {
+    "revision": "96e3e1367d878efa8f50",
+    "url": "/docs/api/static/js/runtime-main.28fd50f1.js"
+  }
+]);
--- public/docs/api/service-worker.js
+++ public/docs/api/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/docs/api/precache-manifest.5712a4cb8a1c6246d8dfc1984eb008fa.js"
+  "/docs/api/precache-manifest.ea21ac9c8cb18a882ce29f03f701204d.js"
 );
 
 self.addEventListener('message', (event) => {
