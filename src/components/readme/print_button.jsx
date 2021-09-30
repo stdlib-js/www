ba849db1dcbe1b06666955693278c4a7364ae5a6
Planeshifter <pburckhardt@outlook.com>
2021-09-30 16:44:25 -0400

    Add header + footer line to printed documentation

--- src/components/readme/print_button.jsx
+++ src/components/readme/print_button.jsx
@@ -22,48 +22,82 @@ import React from 'react';
 import PrintIcon from '@mui/icons-material/Print';
 
 
-// FUNCTIONS //
-
-/**
-* Callback invoked upon clicking a button to print the current page.
-*
-* @private
-* @param {Object} event - event
-*/
-function onClick( event ) {
-	var theme;
-
-	// Prevent default button behavior:
-	event.preventDefault();
-
-	// Cache the current theme value:
-	theme = document.documentElement.getAttribute( 'data-theme' );
-
-	 // Switch the theme to "light" for printing:
-	document.documentElement.setAttribute( 'data-theme', 'light' );
-
-	// Open the print dialog:
-	window.print();
-
-	// Restore the original theme:
-	document.documentElement.setAttribute( 'data-theme', theme );
-}
-
-
 // MAIN //
 
 /**
 * Component for rendering a button to print the current page.
 *
 * @private
+* @param {Object} props - component properties
+* @param {string} props.version - documentation version
+* @param {string} props.url - resource URL
 * @returns {ReactElement} React element
 */
-function PrintButton() {
+function PrintButton( props ) {
 	return (
 		<button className="print-button" onClick={ onClick } >
 			<PrintIcon fontSize="inherit" aria-hidden="true" /> Print this page
 		</button>
 	);
+
+	/**
+	* Callback invoked upon clicking a button to print the current page.
+	*
+	* @private
+	* @param {Object} event - event
+	*/
+	function onClick( event ) {
+		var referenceNode;
+		var footerNode;
+		var headerNode;
+		var footer;
+		var header;
+		var theme;
+		var url;
+
+		// Prevent default button behavior:
+		event.preventDefault();
+
+		// Cache the current theme value:
+		theme = document.documentElement.getAttribute( 'data-theme' );
+
+		// Switch the theme to "light" for printing:
+		document.documentElement.setAttribute( 'data-theme', 'light' );
+
+		// Add a top margin header to the first page:
+		headerNode = document.createElement( 'span' );
+		headerNode.className = 'print-addendum';
+		header = 'stdlib-js documentation';
+		header += ' - Version: ' + props.version;
+		header += ' - The Stdlib Authors © 2016-2021.'
+		headerNode.innerHTML = header;
+
+		referenceNode = document.getElementById( 'readme' );
+		referenceNode.parentNode.insertBefore( headerNode, referenceNode );
+
+		// Add a bottom margin footer to the last page:
+		footerNode = document.createElement( 'p' );
+		footerNode.className = 'print-addendum';
+		footer = 'Thank you for reading the documentation! If you have any questions, please join the discussion in our ';
+		footer += '<a href="https://gitter.im/stdlib-js/stdlib-js">Gitter chat room</a>.';
+		footer += '<br /><br />';
+		url = window.location.origin + props.url;
+		footer += 'Source: <a href="' + url + '" >' + url + '</a>';
+		footer += ' (printed on: ' + new Date().toLocaleDateString() + ')';
+		footerNode.innerHTML = footer;
+
+		referenceNode = document.getElementById( 'readme' );
+		referenceNode.parentNode.insertBefore( footerNode, referenceNode.nextSibling );
+
+		// Open the print dialog:
+		window.print();
+
+		// Restore the original theme:
+		document.documentElement.setAttribute( 'data-theme', theme );
+
+		headerNode.parentNode.removeChild( headerNode );
+		footerNode.parentNode.removeChild( footerNode );
+	}
 }
 
 
