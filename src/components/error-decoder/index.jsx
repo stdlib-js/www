10caf48c86440b691fdc250e8cc308c5b80551b0
Athan Reines <kgryte@gmail.com>
2022-05-03 17:09:17 -0700

    Add stub error decoder component

---
+++ src/components/error-decoder/index.jsx
@@ -0,0 +1,62 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2021 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// MODULES //
+
+import React, { Fragment } from 'react';
+import ClearIcon from './../icons/close.jsx';
+
+
+// MAIN //
+
+/**
+* Component for decoding error messages.
+*
+* @private
+* @param {Object} props - component properties
+* @param {Callback} props.onClose - callback to invoke upon closing the error decoder
+* @returns {ReactElement} React element
+*/
+function ErrorDecoder( props ) {
+	// TODO: handle query string properties
+	return (
+		<Fragment>
+			<div id="readme" className="readme error-decoder" >
+				<h1>
+					<span>Error Decoder</span>
+					<button
+						className="icon-button"
+						title="Close decoder"
+						aria-label="close"
+						onClick={ props.onClose }
+					>
+						<ClearIcon />
+					</button>
+				</h1>
+				<section>
+					<p>Hello World!</p>
+				</section>
+			</div>
+		</Fragment>
+	);
+}
+
+
+// EXPORTS //
+
+export default ErrorDecoder;
