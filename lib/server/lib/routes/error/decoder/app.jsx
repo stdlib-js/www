45d361b32a1fbdaf929b04066b5fa5b2ef1def59
Athan Reines <kgryte@gmail.com>
2022-05-15 18:50:25 -0700

    Add support for user settings

--- lib/server/lib/routes/error/decoder/app.jsx
+++ lib/server/lib/routes/error/decoder/app.jsx
@@ -36,10 +36,15 @@ var StylesProvider = require( '@mui/styles' ).StylesProvider;
 * @param {string} url - request URL
 * @param {string} version - documentation version
 * @param {Object} content - initial content (for SSR)
+* @param {Object} settings - user settings
+* @param {string} settings.theme - documentation theme
+* @param {string} settings.mode - documentation "mode"
+* @param {string} settings.exampleSyntax - example code syntax
+* @param {string} settings.prevNextNavigation - previous/next package navigation mode
 * @param {Object} context - server-side router context
 * @returns {ReactElement} React element
 */
-function WrappedApp( App, url, version, content, context ) {
+function WrappedApp( App, url, version, content, settings, context ) {
 	return (
 		<StylesProvider>
 			<App
@@ -48,6 +53,12 @@ function WrappedApp( App, url, version, content, context ) {
 				data={ {} }
 				query=''
 				content={ content }
+
+				theme={ settings.theme }
+				mode={ settings.mode }
+				exampleSyntax={ settings.exampleSyntax }
+				prevNextNavigation={ settings.prevNextNavigation }
+
 				context={ context }
 			/>
 		</StylesProvider>
