64081e260ce5ab0c89ebadc0c5ca38fd738200df
Athan Reines <kgryte@gmail.com>
2022-05-18 02:39:15 -0700

    Refactor feedback form and eschew Material UI components

--- src/components/readme/feedback.jsx
+++ src/components/readme/feedback.jsx
@@ -18,19 +18,11 @@
 
 // MODULES //
 
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
-import qs from 'qs';
 import TextField from '@mui/material/TextField';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import ThumbsUpIcon from './../icons/thumbs_up.jsx';
+import ThumbsDownIcon from './../icons/thumbs_down.jsx';
 
 
 // MAIN //
@@ -40,7 +32,7 @@ import Stack from '@mui/material/Stack';
 *
 * @private
 */
-class Feedback extends Component {
+class Feedback extends React.Component {
 	/**
 	* Returns a component for rendering a feedback form.
 	*
@@ -54,7 +46,7 @@ class Feedback extends Component {
 	constructor( props ) {
 		super( props );
 		this.state = {
-			'sentiment': null,
+			'sentiment': '',
 			'explanation': ''
 		};
 	}
@@ -67,7 +59,7 @@ class Feedback extends Component {
 	*/
 	_onSentimentChange = ( event ) => {
 		this.setState({
-			sentiment: event.target.value
+			'sentiment': event.target.value
 		});
 	}
 
@@ -79,7 +71,7 @@ class Feedback extends Component {
 	*/
 	_onExplanationChange = ( event ) => {
 		this.setState({
-			explanation: event.target.value
+			'explanation': event.target.value
 		});
 	}
 
@@ -90,9 +82,119 @@ class Feedback extends Component {
 	*/
 	_cancelSubmission = () => {
 		this.setState({
-			'sentiment': null,
+			'sentiment': '',
 			'explanation': ''
 		});
+	}
+
+	/**
+	* Callback invoked upon submitting the feedback form.
+	*
+	* @private
+	* @param {Object} event - event object
+	*/
+	_onSubmit = ( event ) => {
+		event.preventDefault();
+
+		// TODO: add AJAX logic => https://formsubmit.co/7a22568a376a058e7a22f1e4d8eead4a
+		console.log( 'Form submitted' );
+	}
+
+	/**
+	* Renders form elements for indicating positive or negative sentiment.
+	*
+	* @private
+	* @returns {ReactElement} React element
+	*/
+	_renderSentiment() {
+		return (
+			<fieldset>
+				<legend>Did you find this page helpful?</legend>
+				<div
+					className={ 'readme-feedback-sentiment-wrapper ' + ( ( this.state.sentiment ) ? 'readme-feedback-has-sentiment' : 'readme-feedback-no-sentiment' ) }
+					role="radiogroup"
+					aria-label="sentiment"
+				>
+					<input
+						type="radio"
+						name="readme-feedback-sentiment"
+						id="readme-feedback-positive-sentiment"
+						className="readme-feedback-sentiment-radio"
+						value="positive"
+						title="Yes, I found this page helpful."
+						checked={ this.state.sentiment === 'positive' }
+						onChange={ this._onSentimentChange }
+					/>
+					<label
+						htmlFor="readme-feedback-positive-sentiment"
+						className="disable-select readme-feedback-sentiment-label readme-feedback-positive-sentiment"
+					>
+						<ThumbsUpIcon/>
+						<span className="visually-hidden">Yes</span>
+					</label>
+
+					<input
+						type="radio"
+						name="readme-feedback-sentiment"
+						id="readme-feedback-negative-sentiment"
+						className="readme-feedback-sentiment-radio"
+						value="negative"
+						title="No, I didn't find this page helpful."
+						checked={ this.state.sentiment === 'negative' }
+						onChange={ this._onSentimentChange }
+					/>
+					<label
+						htmlFor="readme-feedback-negative-sentiment"
+						className="disable-select readme-feedback-sentiment-label readme-feedback-negative-sentiment"
+					>
+						<ThumbsDownIcon/>
+						<span className="visually-hidden">No</span>
+					</label>
+				</div>
+			</fieldset>
+		);
+	}
+
+	/**
+	* Renders form elements for submitting feedback.
+	*
+	* @private
+	* @returns {ReactElement} React element
+	*/
+	_renderSubmit() {
+		return (
+			<Fragment>
+				<h3>Thank you for your feedback!</h3>
+				<TextField
+					name="explanation"
+					label={ this.state.sentiment === 'positive' ? 'What do you like about this page?' : 'How can we improve this page?' }
+					multiline
+					rows={ 3 }
+					maxRows={ 12 }
+					value={ this.state.explanation }
+					onChange={ this._onExplanationChange }
+					fullWidth
+				/>
+				<div className="readme-feedback-submit-buttons">
+					<button
+						type="button"
+						className="readme-feedback-cancel-button"
+						onClick={ this._cancelSubmission }
+					>
+						Cancel
+					</button>
+					<button
+						type="submit"
+						className="readme-feedback-submit-button"
+					>
+						Submit
+					</button>
+				</div>
+				<p>
+					<sup>*</sup>Please note that we can't reply to your submitted feedback. If you have questions, please reach out to us over chat!
+				</p>
+			</Fragment>
+		);
 	}
 
 	/**
@@ -102,94 +204,26 @@ class Feedback extends Component {
 	* @returns {ReactElement} React element
 	*/
 	render() {
-		/* eslint-disable no-restricted-globals */
-		const origin = typeof window !== 'undefined' ? window.location.origin : '';
-		const subject = 'Feedback on documentation for @stdlib/' + this.props.pkg;
 		return (
 			<form
-				className="readme-feedback" action="https://formsubmit.co/7a22568a376a058e7a22f1e4d8eead4a"
-				method="post"
+				className="readme-feedback"
+				onSubmit={ this._onSubmit }
 			>
+				{ this._renderSentiment() }
+				{ ( this.state.sentiment ) ? this._renderSubmit() : null }
+
 				<input type="hidden" name="pkg" value={ this.props.pkg } />
 				<input type="hidden" name="version" value={ this.props.version } />
-				<FormControl component="fieldset">
-					<FormLabel component="legend">Was this documentation useful for you?</FormLabel>
-					<RadioGroup
-						aria-label="sentiment"
-						name="sentiment"
-						row
-						onChange={ this._onSentimentChange }
-					>
-						<FormControlLabel
-							value="positive"
-							control={
-								<Radio
-									checked={ this.state.sentiment === 'positive' }
-									icon={ <ThumbUpIcon /> }
-									checkedIcon={ <ThumbUpIcon color="success" /> }
-								/>
-							}
-							label="Yes" />
-						<FormControlLabel
-							value="negative"
-							control={
-								<Radio
-									checked={ this.state.sentiment === 'negative' }
-									icon={ <ThumbDownIcon /> }
-									checkedIcon={ <ThumbDownIcon color="error" /> }
-								/>
-							}
-							label="No"
-						/>
-					</RadioGroup>
-				</FormControl>
-				{ this.state.sentiment ?
-					<Fragment>
-						<h3>Thank you for your feedback!</h3>
-						<div>
-							<TextField
-								name="explanation"
-								label={ this.state.sentiment === 'positive' ? 'What do you like about this documentation?' : 'What could we improve?' }
-								multiline
-								rows={ 3 }
-								maxRows={ 12 }
-								value={ this.state.explanation }
-								onChange={ this._onExplanationChange }
-								fullWidth
-							/>
-							<Stack
-								direction="row"
-								justify="right"
-								align="right"
-							>
-								<Button
-									variant="text"
-									onClick={ this._cancelSubmission }
-								>
-									Cancel
-								</Button>
-								<Button
-									variant="text"
-									type="submit"
-								>
-									Submit
-								</Button>
-							</Stack>
-						</div>
-					</Fragment> : null
-				}
-				<input type="hidden" name="_next" value={ origin + this.props.url + '?' + qs.stringify({
-					'notification': 'Feedback successfully submitted.'
-				}) } />
+
+				<input type="hidden" name="_subject" value={ '[feedback] Package Documentation: @stdlib/' + this.props.pkg } />
 				<input type="hidden" name="_template" value="table" />
 				<input type="hidden" name="_url" value="https://stdlib.io" />
-				<input type="hidden" name="_captcha" value="false" />
-				<input type="text" name="_honey" style={{ display: 'none' }} />
-				<input type="hidden" name="_subject" value={ subject } />
 				<input type="hidden" name="_cc" value="pburckhardt@outlook.com" />
+
+				<input type="hidden" name="_captcha" value="false" />
+				<input className="invisible" type="text" name="_honey" />
 			</form>
 		);
-		/* eslint-enable no-restricted-globals */
 	}
 }
 
