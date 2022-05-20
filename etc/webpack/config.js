897949c501fa4280f8611099b842cf0633bdbc69
Athan Reines <kgryte@gmail.com>
2022-05-19 20:27:27 -0700

    Update list of excludes to exclude HTML

--- etc/webpack/config.js
+++ etc/webpack/config.js
@@ -583,7 +583,7 @@ module.exports = function(webpackEnv) {
 			isEnvProduction &&
 				new WorkboxWebpackPlugin.GenerateSW({
 					clientsClaim: true,
-					exclude: [/\.map$/, /asset-manifest\.json$/],
+					exclude: [/\.map$/, /\.html$/, /asset-manifest\.json$/],
 					importWorkboxFrom: 'cdn',
 					navigateFallback: publicUrl + '/index.html',
 					navigateFallbackBlacklist: [
