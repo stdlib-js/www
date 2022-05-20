dd92677423837885722c86c05a71661a71127f7d
Athan Reines <kgryte@gmail.com>
2022-05-19 18:07:46 -0700

    Disable service work registration

---
+++ public/docs/api/precache-manifest.e2177d0aa38f550f317d6e13e34085a9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25ba3bd47736fcbfab6a8592f2eb659",
+    "url": "/docs/api/index.html"
+  },
+  {
+    "revision": "487e4af33cdbed2bed30",
+    "url": "/docs/api/static/js/2.9d6b380d.chunk.js"
+  },
+  {
+    "revision": "480a167b729bbbded20e",
+    "url": "/docs/api/static/js/main.dd2c9167.chunk.js"
+  },
+  {
+    "revision": "96e3e1367d878efa8f50",
+    "url": "/docs/api/static/js/runtime-main.28fd50f1.js"
+  }
+]);
--- public/docs/api/service-worker.js
+++ public/docs/api/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/docs/api/precache-manifest.163b5df8d5bf725e77c973f67a452464.js"
+  "/docs/api/precache-manifest.e2177d0aa38f550f317d6e13e34085a9.js"
 );
 
 self.addEventListener('message', (event) => {
