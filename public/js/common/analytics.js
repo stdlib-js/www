fd96cb659f7697981c4f9f471fab7477e60ae6cd
Athan <kgryte@gmail.com>
2026-06-19 19:21:47 -0700

    fix: use correct property

--- public/js/common/analytics.js
+++ public/js/common/analytics.js
@@ -337,7 +337,7 @@ SOFTWARE.
 		_paq.push( [ 'trackPageView' ] );
 		_paq.push( [ 'enableLinkTracking' ] );
 		(function() {
-			var u = 'https://analytics.stdlib.io/';
+			var u = 'https://stdlib.io/';
 			_paq.push( [ 'setTrackerUrl', u+'matomo.php' ] );
 			_paq.push( [ 'setSiteId', '2' ] );
 			var d = document;
