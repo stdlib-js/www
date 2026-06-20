eae326891355f2a1b0902f8c7b09e67e5f8be692
Athan <kgryte@gmail.com>
2026-06-19 20:13:11 -0700

    refactor: add tag manager

---
+++ public/js/common/tag_manager.js
@@ -0,0 +1,15 @@
+(function() {
+	var _mtm = window._mtm = window._mtm || [];
+	_mtm.push({
+		'mtm.startTime': new Date().getTime(),
+		'event': 'mtm.Start'
+	});
+	(function() {
+		var d = document;
+		g = d.createElement( 'script' );
+		s = d.getElementsByTagName( 'script' )[ 0 ];
+		g.async = true;
+		g.src = 'https://analytics.stdlib.io/js/container_GVCkbCJi.js';
+		s.parentNode.insertBefore( g, s );
+	})();
+})();
